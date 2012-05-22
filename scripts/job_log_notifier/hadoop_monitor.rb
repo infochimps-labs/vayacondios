b8f8501fd1aa8d6448ab611a665417cc71c85f0e
Josh Bronson <josh.bronson@gmail.com>
2012-05-22 15:17:57 -0500

    refactor of job log parser and addition of hadoop monitor

---
+++ scripts/job_log_notifier/hadoop_monitor.rb
@@ -0,0 +1,124 @@
+#!/usr/bin/env jruby
+
+require_relative 'job_log_parser'
+require 'swineherd-fs'
+require 'java'
+require 'mongo'
+
+module Vayacondios
+
+  class HadoopMonitor
+
+    include JobLogParser
+
+    SLEEP_SECONDS = 1
+    MONGO_JOBS_DB = 'job_info'
+    MONGO_JOB_LOGS_COLLECTION = 'job_logs'
+
+    def initialize
+      Swineherd.configure_hadoop_jruby
+      conf = Swineherd.get_hadoop_conf
+      addr = Java::org.apache.hadoop.mapred.JobTracker.get_address conf
+      @job_client = 
+        Java::org.apache.hadoop.mapred.JobClient.new addr, conf
+      @launched_jobs = {}
+      @conn = Mongo::Connection.new
+      @db = @conn[MONGO_JOBS_DB]
+      @coll = @db[MONGO_JOB_LOGS_COLLECTION]
+    end
+
+    def run
+      include_class [org.apache.hadoop.mapred.JobProfile,
+                     java.io.DataOutputStream,
+                     java.io.ByteArrayOutputStream]
+
+      while true
+        sleep SLEEP_SECONDS
+
+        @job_client.get_all_jobs.select do |job_status|
+          job_status.get_run_state ==
+            Java::org.apache.hadoop.mapred.JobStatus::RUNNING
+        end.each do |job_status|
+          job = @job_client.get_job job_status.get_job_id
+
+          unless @launched_jobs[job.get_id.to_s]
+            @launched_jobs[job.get_id.to_s] = job
+            Thread.new(job, job_status) do |job, job_status|
+              begin
+                jp = JobProfile.new(job_status.get_username,
+                                    job.get_id,
+                                    job.get_job_file,
+                                    job.get_tracking_url,
+                                    job.get_job_name)
+
+                bs = ByteArrayOutputStream.new
+                jp.write DataOutputStream.new bs
+
+                # The data format below is reversed from the
+                # undocumented JobProfile.write method. See the
+                # Hadoop source for details.
+
+                # read four-byte big-endian integer that represents
+                # last four digits of job ID. 
+                job_profile_out = bs.to_s
+                job_number = job_profile_out.unpack("N").first
+
+                # Read the rest, which consists of one-byte lengths
+                # and strings of that length.
+                cur = 4; values = []
+                while cur < job_profile_out.size
+                  len = job_profile_out[cur].bytes.first
+                  values << job_profile_out[cur+1..cur+len]
+                  cur += (len + 1)
+                end
+
+                # And here are the values. These are all Text values
+                # in Hadoop.
+                job_prefix, job_file, url, user, name, queue_name = values
+
+                # Grab the properties
+                fs = Swineherd::FileSystem.get job_file
+
+                # There is a small chance that this will cause a
+                # file-not-found exception due to a job completing
+                # between the check for running status above and
+                # here. The correct behavior is just to print a
+                # stack trace, because the java.io.IOException that
+                # Hadoop returns can mean a lot of different
+                # things. At any rate, if that exception comes up,
+                # it's probably something else anyway.
+                properties = JobLogParser.parse_properties(fs.open(job_file))
+
+                output_dir = properties["mapred_output_dir"]
+
+                puts "waiting until #{job.get_id.to_s} is complete"
+                job.wait_for_completion
+                puts "#{job.get_id.to_s} is complete!"
+
+                JobLogParser.parse_log_dir(output_dir, properties).each do |e|
+                  require 'pp'; pp e if e.keys.any?{|k| k.index '.'}
+                  begin
+                    @coll.insert e
+                  rescue BSON::InvalidKeyName => ex
+                    puts "Here's the bad record:"
+                    require 'pp'; pp [e, ex]
+                    #raise
+                  end
+                end
+
+                puts "Done writing updating logs for #{job.get_id.to_s}."
+              rescue Exception => e
+                # Ruby rescues exceptions and fails silently in
+                # threaded code, which is not the behavior we want.
+                puts e
+                puts e.backtrace
+                raise
+              end
+            end
+          end
+          
+        end
+      end
+    end
+  end
+end
--- scripts/job_log_notifier/job_log_parser.rb
+++ scripts/job_log_notifier/job_log_parser.rb
@@ -7,199 +7,175 @@ require 'logger'
 require 'pp'
 require 'gorillib/hash/compact'
 require 'gorillib/string/inflections'
+require 'swineherd-fs'
 
-def parse_cmdline(argv)
+module Vayacondios
 
-  config = OpenStruct.new
+  module JobLogParser
 
-  config.hadoop_output_dir = argv.first
-  if config.hadoop_output_dir[-1] == '/' then
-    config.hadoop_output_dir = config.hadoop_output_dir[0..-2]
-  end
+    public
+    
+    #
+    # dir: output directory of a hadoop job containing _logs
+    # properties: hash containing default properties
+    # 
+    # returns: a list of hashes. One hash represents the job
+    # configuration, and every other hash represents an entry in the
+    # log.
+    # 
+    def self.parse_log_dir dir, properties = nil
+      # find logs
+      dir = File.join dir, "_logs/history"
+      conf, log = ["*.xml", "*{[^x]??,[^m]?,[^l]}"].map do |g|
+        fs = Swineherd::FileSystem.get dir
+        fs.open fs.glob(File.join(dir, g)).first
+      end
 
-  m = /(^(?:[[:alnum:]]+:\/\/|\/)[[:alnum:].]+\/)/i.
-    match(config.hadoop_output_dir)
-  config.hadoop_prefix = m[1] if m
+      parse_log_and_conf log, (properties || conf)
+    end
+    
+    #
+    # log: stream of a hadoop log
+    # conf: stream of a hadoop job conf, or a hash containing
+    #       properties
+    # 
+    # returns: a list of hashes. One hash represents the job
+    # configuration, and every other hash represents an entry in the
+    # log.
+    # 
+    def self.parse_log_and_conf log, properties
+      properties = parse_properties properties unless properties.is_a? Hash
 
-  return config
+      # prime read loop
+      jobid = "unknown"
+      record_hashes = {}
+      %w[meta job task map_attempt reduce_attempt].each do |type|
+        record_hashes[type] = {}
+      end
+      
+      # read loop
+      log.read.scan /(#{scan_until '.'})\./ do |raw_record,|
+        raw_record.strip!
 
-end
+        # parse each record
+        type,l = raw_record.strip.split ' ', 2
+        type = parse_key type
+        record = {}
+        l.scan(/([A-Z_]+)="(#{scan_until '"'})"/m).each do |k,v|
+          if k.downcase.end_with? "counters" then
+            v = parse_counters v
+          else
+            v = parse_atom v
+          end
+          record[parse_key k] = v
+        end
 
-def parse_key key
-  return (parse_atom key).underscore
-end
+        # Decide what fields are ids based on type
+        case type
+        when 'job'
+          idfield = 'jobid'
+          jobid = record[idfield]
+        when 'task'
+          idfield = 'taskid'
+          parent_prefix = 'job_'
+          parent_id_regex = /\d+_\d+/
+        when lambda {|x| x.end_with? 'attempt'}
+          idfield = 'task_attempt_id'
+          parent_prefix = 'task_'
+          parent_id_regex = /\d+_\d+_._\d+/
+        else
+          next
+        end
 
-def parse_atom a
+        # Eliminate duplicate records and set up parent ids.
+        cur = record_hashes[type][record[idfield]] = record
+        cur['_id'] = cur[idfield]
+        cur['parent_id'] = parent_prefix + cur[idfield][/#{parent_id_regex}/] if
+          parent_prefix
+        cur.delete idfield
+        cur['type'] = type
+        
+      end
+      
+      # return all records and a properties hash
+      record_hashes.values.map{|hash| hash.values}.flatten << {
+        'parent' => jobid,
+        'type' => 'conf',
+        'properties' => properties,
+        '_id' => jobid + "_properties"
+      }
+      
+    end
 
-  if /[0-9][ \r\t\n]*\/[ \r\t\n]*[0-9]+/.match a
-    return a.split("/").collect{|s| s.to_i}
-  elsif /^[0-9,]*$/.match a
-    return a.gsub(',', '').to_i
-  else
-    return a.gsub(/([^\\])\\(.)/, '\1\2')
-  end
-end
+    #
+    # Return a hash containing a name => value hash representing the
+    # config for a hadoop job.
+    #
+    def self.parse_properties conf
+      properties = {}
+      conf.read.scan /[^\n]*\n/ do |line,|
+        m = /<name>([^<]+)<\/name><value>([^<]+)<\/value>/.match line
+        if m and m[1] !~ /fs\.s3n?\.awsSecretAccessKey/ then
+          properties[parse_key m[1]] = parse_atom m[2]
+        end
+      end
+      properties
+    end
 
-def scan_until b
-  return /(?:[^\\#{b}]|\\.)*/
-end
+    private
 
-def parse_counters counters
-  result = {}
-  counters.scan /{(#{scan_until '\['})(#{scan_until "\}"})}/ do |headers,table|
-    headers.scan(/\((#{scan_until ')'})\)
-                  \((#{scan_until ')'})\)/x) do |type, description|
-      type = parse_key type
-      result[type] = {}
-      table.scan(/\[\((#{scan_until ')'})\)
-                    \((#{scan_until ')'})\)
-                    \((#{scan_until ')'})\)\]/x) do |name,description,val|
-        result[type][parse_key name] = parse_atom val
+    #
+    # Parse a key in a log entry. Log entries consist of a type, which I
+    # consider a key, and a list of key=value pairs.
+    #
+    def self.parse_key key
+      return (parse_atom key).underscore.gsub ".", "_"
+    end
+
+    #
+    # Parse a value in a Hadoop log.
+    #
+    def self.parse_atom a
+      if /[0-9][ \r\t\n]*\/[ \r\t\n]*[0-9]+/.match a
+        # "0/6" -> [0,6]
+        return a.split("/").collect{|s| s.to_i}
+      elsif /^[0-9,]*$/.match a
+        # "224" -> 224
+        return a.gsub(',', '').to_i
+      else
+        # \. -> .
+        return a.gsub(/([^\\])\\(.)/, '\1\2')
       end
     end
-  end
-  return result
-end
 
-def each_record log
-  log.scan /(#{scan_until '.'})\./ do |record,|
-    record.strip!
-    yield parse_record(record) if record.size > 0
-  end
-end
-
-def parse_record line
-  type,l = line.strip.split ' ', 2
-  type = parse_key type
-  cur = {}
-  l.scan(/([A-Z_]+)="(#{scan_until '"'})"/m).each do |k,v|
-    if k.downcase.end_with? "counters" then
-      v = parse_counters v
-    else
-      v = parse_atom v
-    end
-    cur[parse_key k] = v
-  end
-  return {"type"=>type,"record"=>cur}
-end
-
-def init_hsh(hsh, key, mk_att_key = true)
-  hsh[key] = if hsh[key] == nil then
-               {'attempts' => ({} if mk_att_key), 'info' => {}}.compact
-             else
-               hsh[key]
-             end
-end
-
-def parsed_records log
-  job_info = {'info' => {}, 'tasks' => {}}
-  jobid = nil
-  each_record(log) do |record|
-    case record['type']
-    when 'job'
-      cur = job_info['info']
-      parent = nil
-      idkey = 'jobid'
-      jobid = record['record']['jobid']
-    when 'task'
-      taskid = record['record']['taskid']
-      parent = job_info['info']['_id']
-      cur = init_hsh(job_info['tasks'], taskid)['info']
-      idkey = 'taskid'
-    when lambda {|x| x.end_with? 'attempt'}
-      attid = record['record']['task_attempt_id']
-      parent = taskid = record['record']['taskid']
-
-      cur = init_hsh(init_hsh(job_info['tasks'], taskid)['attempts'], attid, false)['info']
-      idkey = 'task_attempt_id'
-    else
-      next
+    #
+    # returns a regex that matches until character b. Escaped b's are
+    # skipped over.
+    #
+    def self.scan_until b
+      return /(?:[^\\#{b}]|\\.)*/
     end
 
-    cur['type'] = record['type']
-    cur['_id'] = record['record'][idkey]
-    cur['parent_id'] = parent if parent
-    record['record'].delete idkey
-    record['record'].each do |k,v|
-      cur[k] = v
+    #
+    # parses counters in a Hadoop log into a hash
+    #
+    def self.parse_counters counters
+      result = {}
+      counters.scan /{(#{scan_until '\['})(#{scan_until "\}"})}/ do |counter|
+        headers, table = counter
+        headers.scan(/\((#{scan_until ')'})\)
+                  \((#{scan_until ')'})\)/x) do |type, description|
+          type = parse_key type
+          result[type] = {}
+          table.scan(/\[\((#{scan_until ')'})\)
+                    \((#{scan_until ')'})\)
+                    \((#{scan_until ')'})\)\]/x) do |name,description,val|
+            result[type][parse_key name] = parse_atom val
+          end
+        end
+      end
+      return result
     end
+
   end
-
-  job_info['tasks'] = (job_info['tasks'].values.collect do |t|
-                         t['attempts'] = t['attempts'].values.collect { |a| a }
-                         t
-                       end)
-
-  tasks = job_info['tasks']
-  job_info.delete 'tasks'
-
-  task_attempts = []
-
-  tasks.each do |t|
-    task_attempts << t['attempts']
-    task_attempts.flatten!
-    t.delete 'attempts'
-  end
-
-  return [[job_info] + tasks + task_attempts, jobid]
 end
-
-conf = parse_cmdline ARGV
-
-def find_files(files)
-  log_file, conf_file = nil, nil
-  
-  files.each do |p|
-    p.strip!
-    if p.end_with? '.xml'
-    then
-      conf_file = p
-    elsif not p.end_with? '.'
-      log_file = p
-    end
-  end
-
-  return log_file, conf_file
-end
-
-def parse_properties(conf_lines, jobid)
-  properties = {}
-  conf_lines.each do |line|
-    m = /<name>([^<]+)<\/name><value>([^<]+)<\/value>/.match line
-    if m and not m[1].downcase.index('secret') then
-      properties[m[1]] = m[2]
-    end
-  end
-  return {'parent' => jobid, 'type' => 'conf', 'properties' => properties}
-end
-
-if conf.hadoop_prefix then
-  proc = IO.popen("hadoop fs -ls #{conf.hadoop_output_dir}/_logs/history",
-                  :err => ['/dev/null'])
-  log_file, conf_file = find_files(proc.readlines[1..-1].
-                                   map {|l| l.split('/', 2)[-1]})
-  
-  log = IO.popen("hadoop fs -cat #{conf.hadoop_prefix}/#{log_file}",
-                 :err => ['/dev/null']).read
-  conf = IO.popen("hadoop fs -cat #{conf.hadoop_prefix}/#{conf_file}",
-                  :err => ['/dev/null']).readlines
-
-else
-  log_file, conf_file = find_files(Dir.new(File.join(conf.hadoop_output_dir,
-                                                     "_logs/history")).entries)
-
-  log = open(File.join(conf.hadoop_output_dir, "_logs/history/#{log_file}"),
-             :err => ['/dev/null']).read
-  conf = open(File.join(conf.hadoop_output_dir, "_logs/history/#{conf_file}"),
-              :err => ['/dev/null']).readlines
-end
-
-job_info = {'info' => {}, 'tasks' => {}}
-
-records, jobid = parsed_records(log)
-
-records.each do |entry|
-    puts JSON.generate(entry['info'])
-end
-
-puts JSON.generate(parse_properties(conf, jobid))
