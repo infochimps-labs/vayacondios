af419baafeb3ab399b562d324d7d887e282b9511
Travis Dempsey <dempsey.travis@gmail.com>
2012-07-30 15:34:11 -0500

    rename Client => HttpClient

--- lib/vayacondios/configliere.rb
+++ lib/vayacondios/configliere.rb
@@ -1,18 +1,18 @@
-require 'configliere' unless defined?::Configliere
+require 'configliere'
 require 'gorillib/hash/keys'
-require 'vayacondios/client'
+require 'vayacondios/http_client'
 
 class Vayacondios
   module Configliere
     def load_from_vayacondios(organization, id, options = {})
       options.symbolize_keys!.deep_merge!(organization: organization)
 
-      client = ::Vayacondios::Client.receive(options.deep_compact)
+      client = ::Vayacondios::HttpClient.receive(options.deep_compact)
       id     = [id, options[:env]].compact.join('.')
       
       begin
         new_data = client.fetch(:config, id)
-      rescue ::Vayacondios::Client::Error
+      rescue ::Vayacondios::HttpClient::Error
         warn "Unable to load vayacondios config '#{id}' for #{organization} at: #{client.host}:#{client.port}"
         new_data = {}
       end
@@ -23,12 +23,12 @@ class Vayacondios
     def save_to_vayacondios(organization, id, options = {})
       options.symbolize.keys!.deep_merge!(organization: organization)
 
-      client = ::Vayacondios::Client.receive(options.deep_compact)
+      client = ::Vayacondios::HttpClient.receive(options.deep_compact)
       id = [id, options[:env]].compact.join('.')
       
       begin
         client.insert(:config, id)
-      rescue ::Vayacondios::Client::Error
+      rescue ::Vayacondios::HttpClient::Error
         warn "Unable to save vayacondios config '#{id}' for #{organization} at: #{client.host}:#{client.port}"
       end
       self
