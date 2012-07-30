aaa1baa643267182443fbda9c194516038d87339
Travis Dempsey <dempsey.travis@gmail.com>
2012-07-30 15:34:56 -0500

    allowed for include Notifications, added LogNotifier, cleanup

--- lib/vayacondios/notifier.rb
+++ lib/vayacondios/notifier.rb
@@ -1,40 +1,55 @@
+require 'gorillib/builder'
 require 'gorillib/exception/raisers'
+require 'gorillib/logger/log'
 require 'gorillib/metaprogramming/class_attribute'
-require 'vayacondios/client'
+require 'vayacondios/http_client'
 
 class Vayacondios
   class_attribute :notifier
 
-  def self.notify(*args)
-    self.notifier.notify(*args)
-  end
+  module Notifications 
 
+    def notify(topic, cargo = {})
+      notifier.notify(topic, cargo)
+    end
+    
+    def self.included(base)
+      base.class_attribute :notifier
+      base.notifier = Vayacondios.notifier
+    end
+
+  end
+  
+  extend Notifications
+  
   class Notifier < Vayacondios
-    def notify(topic, cargo={})
+    include Gorillib::Builder
+    
+    def notify(topic, cargo = {})
       NoMethodError.unimplemented_method(self)
+    end
+  end
+  
+  class LogNotifier < Notifier
+    field :client, Whatever, :default => Log
+    
+    def notify(topic, cargo = {})
+      level    = cargo.delete(:level) || :info
+      message  = "Notification: #{topic.inspect}."
+      message += " Reason: #{cargo.delete(:reason)}." if cargo[:reason]
+      message += " Cargo: #{cargo.inspect}"
+      client.send(level, message)
     end
   end
 
   class HttpNotifier < Notifier
-    def initialize(options={})
-      options = Settings.vayacondios.merge(options) if defined?(Settings)
-  
-      @host         = options[:host]
-      @port         = options[:port]
-      @organization = options[:organization]
-  
-      @client       =  ::Vayacondios::Client.new(@host, @port, @organization)
-    end
-  
-    def notify(topic, cargo={})
-      raise "Cannot notify with unset host" if @host.nil?
-      raise "Cannot notify with unset port" if @port.nil?
-      raise "Cannot notify with unset organization" if @organization.nil?
-      
-      @client.insert(cargo, :event, topic)
+    field :client, Vayacondios::HttpClient
+    
+    def notify(topic, cargo = {})      
+      client.insert(cargo, :event, topic)
       nil
     end
   end
 
-  self.notifier = HttpNotifier.new
+  self.notifier = HttpNotifier.receive(client: {})
 end
