fa635a7d55723f3cfa390cef4d75e3bf27657023
Logan Lowell <fractaloop@gmail.com>
2012-08-28 15:35:48 -0500

    Adding deep merge spec (currently broken)

--- spec/server/config_spec.rb
+++ spec/server/config_spec.rb
@@ -42,5 +42,31 @@ describe HttpShim do
         end
       end
     end
+    
+    it 'merge deep configuration' do
+      with_api(HttpShim) do |api|
+        post_request({
+          :path => '/v1/infochimps/config/mergetest',
+          :body => { :foo => { :bar => 3 } }
+        }, err)
+      end
+      with_api(HttpShim) do |api|
+        post_request({
+          :path => '/v1/infochimps/config/mergetest',
+          :body => { :foo => { :baz => 7 } }
+        }, err)
+      end
+      with_api(HttpShim) do |api|
+        get_request({:path => '/v1/infochimps/config/mergetest'}, err) do |c|
+          c.response_header.status.should == 200
+          MultiJson.load(c.response).should eql({
+            "foo" => {
+              "bar" => 3,
+              "baz" => 7
+            }
+          })
+        end
+      end
+    end
   end
 end
