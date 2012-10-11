ea9e36e119b2395f4a7509ede25e938018628606
Travis Dempsey <dempsey.travis@gmail.com>
2012-10-11 11:54:45 -0500

    shim uses rack abstracted code now

--- app/http_shim.rb
+++ app/http_shim.rb
@@ -1,98 +1,48 @@
 #!/usr/bin/env ruby
 
 require 'vayacondios-server'
-require 'multi_json'
 
 class HttpShim < Goliath::API
-  use Goliath::Rack::Tracer, 'X-Tracer' # log trace statistics
-  use Goliath::Rack::DefaultMimeType    # cleanup accepted media types
-  use Goliath::Rack::Formatters::JSON   # JSON output formatter
-  use Goliath::Rack::Render             # auto-negotiate response format
-  use Goliath::Rack::Heartbeat          # respond to /status with 200, OK (monitoring, etc)
+  use Goliath::Rack::Tracer, 'X-Tracer'                                  # log trace statistics
+  use Goliath::Rack::Params                                              # parse query string and message body into params hash
+  use Goliath::Rack::Validation::RequestMethod, %w[GET PUT PATCH DELETE] # only allow these methods
+  use Vayacondios::Rack::ExtractMethods                                  # interpolate GET, PUT into :create, :update, etc
+  use Vayacondios::Rack::Path                                            # parse path into parameterized pieces
+  use Vayacondios::Rack::PathValidation                                  # validate the existence of env[:vayacondios_path]
+  use Goliath::Rack::Formatters::JSON                                    # JSON output formatter
+  use Goliath::Rack::Render                                              # auto-negotiate response format
+  use Goliath::Rack::Heartbeat                                           # respond to /status with 200, OK (monitoring, etc)
 
   def response(env)
-    # Validate path_params
-    path_params = parse_path(env[Goliath::Request::REQUEST_PATH])
-    return [400, {}, MultiJson.dump({error: "Bad Request"})] if !path_params.present? || method_name(env).nil?
-
-    # TODO refactor a middlware
-    # Decode the document body
-    body = nil
-    begin
-      if env['rack.input']
-        body = env['rack.input'].read
-        body = MultiJson.decode(body) if !body.blank?
-        env['rack.input'].rewind
-      end
-    rescue MultiJson::DecodeError => ex
-      return [400, {}, MultiJson.dump({error: "Bad Request"})]
-    end
-    # Look up handler using inflection
+    path_params = env[:vayacondios_path]
     klass = ('vayacondios/' + path_params[:type] + '_handler').camelize.constantize
 
     begin
-      case method_name(env)
-
+      case env[:vayacondios_method]
+        
       when :show
         record = klass.find(mongo, path_params)
         [200, {}, MultiJson.dump(record.body)]
 
       when :update
-        record = klass.new(mongo).update(body, path_params)
+        record = klass.new(mongo).update(env['params'], path_params)
         [200, {}, nil]
         
       when :patch
-        record = klass.new(mongo).patch(body, path_params)
+        record = klass.new(mongo).patch(env['params'], path_params)
         [200, {}, nil]
 
       when :delete
-        record = klass.find(mongo, path_params).destroy(body)
+        record = klass.find(mongo, path_params).destroy(env['params'])
         [200, {}, MultiJson.dump(record.body)]
-      
-      when :create
-        return [405, ({'Allow' => "GET PUT PATCH DELETE"}), nil]
       end
     rescue Vayacondios::Error::NotFound => ex
-      return [404, {}, MultiJson.dump({error: "Not Found"})]
+      return [404, {}, MultiJson.dump({ error: "Not Found" })]
     rescue Vayacondios::Error::BadRequest => ex
-      return [400, {}, MultiJson.dump({error: "Bad Request"})]
+      return [400, {}, MultiJson.dump({ error: "Bad Request" })]
     rescue StandardError => ex
       puts ex
-      ex.backtrace.each{|l| puts l}
-    end
-  end
-
-  private
-
-  # Determine the organization, type of action (config or event), the topic,
-  # id, and format for the request.
-  def parse_path path
-    path_regex = /^\/v1\/(?<organization>[a-z]\w+)\/(?<type>config|event|itemset)(\/(?<topic>\w+)(\/(?<id>(\w+\/?)+))?)?(\/|\.(?<format>json))?$/i
-    if (match = path_regex.match(path))
-      {}.tap do |segments|
-        match.names.each do |segment|
-          segments[segment.to_sym] = match[segment]
-        end
-      end
-    end
-  end
-
-  def method_name env
-    case env['REQUEST_METHOD'].upcase
-    when "GET"
-      :show
-    when "PUT"
-      if env['HTTP_X_METHOD'] && env['HTTP_X_METHOD'].upcase == 'PATCH'
-        :patch
-      else
-        :update
-      end
-    when "POST"
-      :create
-    when "PATCH"
-      :patch
-    when "DELETE"
-      :delete
+      ex.backtrace.each{ |l| puts l }
     end
   end
 end
