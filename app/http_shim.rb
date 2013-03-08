03e681e2f8486c8e416063a7298ae7cda39cdeb8
Josh Bronson <josh.bronson@gmail.com>
2013-03-07 21:00:49 -0600

    bug fix for server

--- app/http_shim.rb
+++ app/http_shim.rb
@@ -3,7 +3,6 @@
 require 'vayacondios-server'
 
 class HttpShim < Goliath::API
-  use Vayacondios::Rack::AssumeJSON                                      # assume application/json content type
   use Goliath::Rack::Tracer, 'X-Tracer'                                  # log trace statistics
   use Goliath::Rack::Params                                              # parse query string and message body into params hash
   use Goliath::Rack::Validation::RequestMethod, %w[GET PUT PATCH DELETE] # only allow these methods
