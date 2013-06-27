d7a0ec8d8f6245fb925bfba4a6e57ae063438693
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-06-27 14:02:21 -0500

    cleaner announces counts of records it cleaned back to Vayacondios

--- spec/server/cleaner_spec.rb
+++ spec/server/cleaner_spec.rb
@@ -2,12 +2,45 @@ require 'spec_helper'
 require 'vayacondios/server/cleaner'
 
 describe Vayacondios::Cleaner do
-
+  
   let(:now) { Time.now }
-  before    { Timecop.freeze(now) }
+
+  let(:database)   { mock("Mongo::Database")   }
+
+  let(:query)      { {t: { :$gte => now } }    }
+
+  let(:events)     { mock("Mongo::Collection events") }
+  let(:org1topic1) { mock("Mongo::Collection org1topic1") }
+  let(:org1topic2) { mock("Mongo::Collection org1topic2") }
+  let(:org2topic1) { mock("Mongo::Collection org2topic1") }
+  let(:org1)       { mock("Mongo::Collection org1") }
+  let(:org2)       { mock("Mongo::Collection org2") }
+  let(:system)     { mock("Mongo::Collection system") }
+  let(:other)      { mock("Mongo::Collection other") }
+  
+  before do
+
+    Timecop.freeze(now)
+    
+    subject.stub!(:database).and_return(database)
+    
+    database.stub!(:collection_names).and_return(%w[ org1.topic1.events org1.topic2.events org2.topic1.events org1.stash org2.stash vayacondios.vayacondios_cleaner.events system.indexes other ])
+    database.stub!(:collection).with('org1.topic1.events').and_return(org1topic1)
+    database.stub!(:collection).with('org1.topic2.events').and_return(org1topic2)
+    database.stub!(:collection).with('org2.topic1.events').and_return(org2topic1)
+    database.stub!(:collection).with('org1.stash').and_return(org1)
+    database.stub!(:collection).with('org2.stash').and_return(org2)
+    database.stub!(:collection).with('vayacondios.vayacondios_cleaner.events').and_return(events)
+    database.stub!(:collection).with('system.indexes').and_return(system)
+    database.stub!(:collection).with('other').and_return(other)
+    
+    $stdout.stub!(:puts)
+    $stderr.stub!(:puts)
+  end
+  
   after     { Timecop.return      }
 
-  subject { Vayacondios:: Cleaner.new }
+  subject { Vayacondios::Cleaner.new }
 
   describe "#validate" do
     it "raises an error when no --from or --upto are specified" do
@@ -45,36 +78,9 @@ describe Vayacondios::Cleaner do
 
   describe "#clean" do
 
-    let(:database)   { mock("Mongo::Database")   }
+    before { subject.stub!(:from).and_return(now) }
 
-    let(:query)      { {t: { :$gte => now } }    }
-      
-    let(:org1topic1) { mock("Mongo::Collection") }
-    let(:org1topic2) { mock("Mongo::Collection") }
-    let(:org2topic1) { mock("Mongo::Collection") }
-    let(:org1)       { mock("Mongo::Collection") }
-    let(:org2)       { mock("Mongo::Collection") }
-    let(:system)     { mock("Mongo::Collection") }
-    let(:other)      { mock("Mongo::Collection") }
-    
-    before do
-      subject.stub!(:database).and_return(database)
-      subject.stub!(:from).and_return(now)
-      
-      database.stub!(:collection_names).and_return(%w[ org1.topic1.events org1.topic2.events org2.topic1.events org1.stash org2.stash system.indexes other ])
-      database.stub!(:collection).with('org1.topic1.events').and_return(org1topic1)
-      database.stub!(:collection).with('org1.topic2.events').and_return(org1topic2)
-      database.stub!(:collection).with('org2.topic1.events').and_return(org2topic1)
-      database.stub!(:collection).with('org1.stash').and_return(org1)
-      database.stub!(:collection).with('org2.stash').and_return(org2)
-      database.stub!(:collection).with('system.indexes').and_return(system)
-      database.stub!(:collection).with('other').and_return(other)
-      
-      $stdout.stub!(:puts)
-      $stderr.stub!(:puts)
-    end
-
-    after { subject.clean }
+    after  { subject.clean }
 
     context "with no --matching argument" do
       context "in normal mode" do
@@ -82,6 +88,7 @@ describe Vayacondios::Cleaner do
           org1topic1.should_receive(:remove).with(query).and_return({'n' => 1})
           org1topic2.should_receive(:remove).with(query).and_return({'n' => 1})
           org2topic1.should_receive(:remove).with(query).and_return({'n' => 1})
+          events.should_receive(:remove).with(query).and_return({'n' => 1})
         end
       end
       
@@ -91,6 +98,7 @@ describe Vayacondios::Cleaner do
           org1topic1.should_receive(:find).with(query).and_return(mock("Mongo::Cursor", count: 1))
           org1topic2.should_receive(:find).with(query).and_return(mock("Mongo::Cursor", count: 1))
           org2topic1.should_receive(:find).with(query).and_return(mock("Mongo::Cursor", count: 1))
+          events.should_receive(:find).with(query).and_return(mock("Mongo::Cursor", count: 1))
         end
       end
     end
@@ -113,6 +121,49 @@ describe Vayacondios::Cleaner do
         end
       end
     end
+  end
+
+  describe "#finish" do
+
+    let(:counts) {
+      [
+       { collection: 'foo', count:  3},
+       { collection: 'bar', count:  4}
+      ]
+    }
+    
+    before do
+      subject.stub!(:from).and_return(now)
+      subject.stub!(:counts).and_return(counts)
+      events.stub!(:insert)
+    end
+    
+    after  { subject.finish }
+
+    context "in normal mode" do
+      it "announces the counts to Vayacondios" do
+        events.should_receive(:insert).with(t: now, d: counts)
+      end
+
+      it "prints the counts" do
+        $stdout.should_receive(:puts).with("3	foo")
+        $stdout.should_receive(:puts).with("4	bar")
+      end
+    end
+
+    context "in --dry_run mode" do
+      before { subject.settings[:dry_run] = true }
+      it "does not announce an event" do
+        events.should_not_receive(:insert)
+      end
+
+      it "prints the counts" do
+        $stdout.should_receive(:puts).with("3	foo")
+        $stdout.should_receive(:puts).with("4	bar")
+      end
+    end
     
   end
+  
+  
 end
