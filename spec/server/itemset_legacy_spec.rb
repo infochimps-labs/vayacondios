d024ba2251891f0898d6bba77d0ceca983a40dfb
Josh Bronson <josh.bronson@gmail.com>
2013-08-23 17:55:22 -0500

    removed vcd itemset tests that no longer apply

--- spec/server/itemset_legacy_spec.rb
+++ spec/server/itemset_legacy_spec.rb
@@ -107,107 +107,7 @@ describe Vayacondios::HttpServer do
     end
   end
 
-  context 'handles PUT requests in legacy mode' do
-    #Vayacondios.force_legacy_mode(true)
-    it 'only accepts arrays' do
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-                      :path => '/v1/infochimps/itemset/power/level',
-                      :body => MultiJson.dump({'foo' => 'bar'}),
-                      :head => { :content_type => 'application/json' }
-                    }, err) do |c|
-          c.response_header.status.should == 400  # geometrid: changed from 500
-        end
-
-        mongo_query do |db|
-          db.collection("infochimps.itemset").find_one({:_id => "power"}).should be_nil
-        end
-      end
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-                      :path => '/v1/infochimps/itemset/power/level',
-                      :body => "foo",
-                      :head => { :content_type => 'application/json' }
-                    }, err) do |c|
-          c.response_header.status.should == 400
-        end
-
-        mongo_query do |db|
-          db.collection("infochimps.itemset").find_one({:_id => "power"}).should be_nil
-        end
-      end
-    end
-    it "stores the array when the resource doesn't exist" do
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-          :path => '/v1/infochimps/itemset/power/level',
-          :body => MultiJson.dump(["foo", "bar"]),
-          :head => { :content_type => 'application/json' }
-        }, err) do |c|
-          c.response_header.status.should == 200 # TODO Make this 201 Created
-          c.response.should eql ""
-        end
-
-        mongo_query do |db|
-          db.collection("infochimps.power.itemset").find_one({:_id => "level"})["d"].should eql ["foo", "bar"]
-        end
-      end
-    end
-    it "clobbers the previous array when the resource does exist" do
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-          :path => '/v1/infochimps/itemset/power/level',
-          :body => MultiJson.dump(["chimpanzee", "bonobo"]),
-          :head => { :content_type => 'application/json' }
-        }, err) do |c|
-          c.response_header.status.should == 200 # TODO Make this 204 No content
-          c.response.should eql ""
-        end
-      end
-
-      # Verify the first was created
-      mongo_query do |db|
-        db.collection("infochimps.power.itemset").find_one({:_id => "level"})["d"].should eql ["chimpanzee", "bonobo"]
-      end
-
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-          :path => '/v1/infochimps/itemset/power/level',
-          :body => MultiJson.dump(["foo", "bar"]),
-          :head => { :content_type => 'application/json' }
-        }, err) do |c|
-          c.response_header.status.should == 200
-          c.response.should eql ""
-        end
-
-        # Verify the first was clobbered
-        mongo_query do |db|
-          db.collection("infochimps.power.itemset").find_one({:_id => "level"})["d"].should eql ["foo", "bar"]
-        end
-      end
-    end
-  end
-
   context "handles PATCH requests in legacy mode" do
-    #Vayacondios.force_legacy_mode(true)
-    it 'creates a missing resource' do
-      with_api(Vayacondios::HttpServer) do |api|
-        put_request({
-          :path => '/v1/infochimps/itemset/power/level',
-          :head => ({'X-Method' => 'PATCH', :content_type => 'application/json' }),
-          :body => MultiJson.dump(["bar"])
-        }, err) do |c|
-          c.response_header.status.should eql 200 # TODO Make this 201 Created
-          c.response.should eql ""
-        end
-
-        # Verify the resource was created
-        mongo_query do |db|
-          db.collection("infochimps.power.itemset").find_one({:_id => "level"})["d"].should eql ["bar"]
-        end
-      end
-    end
-
     it 'merges with PATCH' do
       with_api(Vayacondios::HttpServer) do |api|
         put_request({
@@ -233,19 +133,6 @@ describe Vayacondios::HttpServer do
   end
 
   context "will handle DELETE requests in legacy mode" do
-    #Vayacondios.force_legacy_mode(true)
-    it 'will not delete a missing resource' do
-      with_api(Vayacondios::HttpServer) do |api|
-        delete_request({
-          :path => '/v1/infochimps/itemset/merge/test',
-          :body => MultiJson.dump(["bar"]),
-          :head => { :content_type => 'application/json' }
-        }, err) do |c|
-          c.response_header.status.should == 404
-        end
-      end
-    end
-
     it "will be ok to delete items that don't exist" do
       with_api(Vayacondios::HttpServer) do |api|
         put_request({
