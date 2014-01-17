75f67e96bd01f92907bcff4a2e5cbc54a5b83cd3
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-17 11:03:45 -0600

    simplified actions and configuration

--- lib/vayacondios/server/drivers/mongo.rb
+++ lib/vayacondios/server/drivers/mongo.rb
@@ -1,18 +1,121 @@
 class Vayacondios
   module Server
     class MongoDriver
-
-      attr_reader :connection
+      include Driver
 
       def self.connect(options = {})
-        new(options).connection
+        new(options)
       end
 
       def initialize(options = {})
+        @log = options[:log]
         mongo = EM::Mongo::Connection.new(options[:host], options[:port], 1, reconnect_in: 1)
-        @connection = mongo.db(options[:name])
+        @connection = mongo.db options[:name]
+      end
+
+      def connection
+        @connection.collection location
+      end
+
+      # Coerce objects into a BSON::ObjectId representation if possible.
+      #
+      # @param [BSON::ObjectId,Hash,#to_s] id the object to be coerced
+      # @return [BSON::ObjectId] the canonical representation of the ID
+      # @raise [Error] if `id` is a Hash and is missing the `$oid` parameter which is expected in this case
+      # @raise [Error] if the String representation of `id` is blank or empty
+      def format_id id
+        case
+        when id.is_a?(BSON::ObjectId)
+          id
+        when id.is_a?(Hash)
+          raise Error.new("When settings the ID of a #{self.class} with a Hash, an '$oid' key is required") if id['$oid'].nil?
+          format_id(id['$oid'])
+        when !id.to_s.empty?
+          id.to_s.match(/^[a-f0-9]{24}$/) ? BSON::ObjectId(id.to_s) : id.to_s
+        else
+          raise Error.new("A #{self} cannot have a blank or empty ID")
+        end
+      end
+
+      def mongo_prepare doc
+        doc[:_id] = format_id(doc[:_id]) if doc[:_id]
+        doc
+      end
+
+      def mongo_unprepare doc
+        doc['_id'] = doc['_id'].to_s
+        doc
+      end
+
+      def selector query
+        sel = { }.tap do |sel|
+          time = query.delete(:_t)
+          sel[:_t] = time.inject({}){ |t, (k,v)| t[('$' + k.to_s).to_sym] = v ; t } if time
+        end
+        query.merge(sel).compact_blank
       end
       
+      def projector query
+        if query[:sort] == 'time'
+          query[:sort] = '_t'
+        elsif query[:sort].present?
+          query[:sort] = '_d.' + query[:sort]
+        end
+        query[:_reverse] if query.delete(:order) == 'descending'
+        query
+      end
+
+      def insert request
+        mongo_doc = mongo_prepare request.document
+        log.debug "    Mongo doc: #{mongo_doc}"
+        res = connection.insert mongo_doc
+        log.debug "      Result: #{res}"
+        { _id: format_id(res).to_s }
+      end
+
+      def retrieve request
+        mongo_doc = mongo_prepare request.document
+        log.debug "    Mongo doc: #{mongo_doc}"
+        res = connection.find_one mongo_doc
+        log.debug "      Result: #{res}"
+        return nil if res.nil?
+        mongo_unprepare res
+      end
+
+      def update request
+        mongo_doc = mongo_prepare request.document
+        log.debug "    Mongo doc: #{mongo_doc}"
+        res = connection.update({ _id: mongo_doc[:_id] }, mongo_doc, { upsert: true })
+        log.debug "      Result: #{res}"
+        res
+      end
+
+      def search(request, opts)
+        select = selector(request.filter)
+        log.debug "    Selector doc: #{select}"
+        project = projector(opts)
+        log.debug "    Projector doc: #{project}"
+        res = connection.find(select, project) || []
+        log.debug "      Result: #{res}"
+        res.map{ |res| mongo_unprepare res }
+      end
+
+      def remove request
+        mongo_doc = mongo_prepare request.document
+        log.debug "    Mongo doc: #{mongo_doc}"
+        res = connection.remove mongo_doc
+        log.debug "      Result: #{res}"
+        nil
+      end
+
+      # for testing only
+      def reset!
+        connection.drop
+      end
+
+      def count
+        connection.count
+      end
     end
   end
 end
