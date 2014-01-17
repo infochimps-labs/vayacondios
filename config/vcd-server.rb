ec271d5ab05f0d97faedd616651b0f15736419e7
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-17 10:58:22 -0600

    use new configuration in server config file

--- config/vcd-server.rb
+++ config/vcd-server.rb
@@ -10,29 +10,28 @@
 # The production environment uses EventMachine::Synchrony to define a
 # shared pool of open connections to the database. The size of this pool
 # can be on the command-line with the --database.connections option.
-db_options = options[:database]
+Vayacondios::Server::DbConfig.overlay options[:database]
 
 environment(:production) do
+  db_options = Vayacondios::Server::DbConfig.env :production
   driver = Vayacondios::Server::Driver.retrieve db_options[:driver]
   logger.info("Opening #{db_options[:connections]} connections to #{db_options[:host]}:#{db_options[:port]} using #{driver}.")
   config['db'] = EventMachine::Synchrony::ConnectionPool.new(size: db_options[:connections]) do
-    driver.connect db_options
+    driver.connect db_options.merge(log: logger)
   end
 end
 
 # The development environment uses a single database connection.
 environment(:development) do
+  db_options = Vayacondios::Server::DbConfig.env :development
   driver = Vayacondios::Server::Driver.retrieve db_options[:driver]
   logger.info("Connecting to #{db_options[:host]}:#{db_options[:port]} using #{driver}.")
-  config['db'] = driver.connect db_options
+  config['db'] = driver.connect db_options.merge(log: logger)
 end
 
-# The test environment does not read its options from the command line
-# but from a Configliere Settings object which is defined.
-#
-# @see spec/spec_helper.rb which defines and initializes the Settings object
 environment(:test) do
-  driver = Vayacondios::Server::Driver.retrieve Settings[:database][:driver]
-  logger.info("Connecting to #{Settings[:database][:host]}:#{Settings[:database][:port]} using #{driver}.")
-  config['db'] = driver.connect Settings[:database]
+  db_options = Vayacondios::Server::DbConfig.env :test
+  driver = Vayacondios::Server::Driver.retrieve db_options[:driver]
+  logger.info("Connecting to #{db_options[:host]}:#{db_options[:port]} using #{driver}.")
+  config['db'] = driver.connect db_options.merge(log: logger)
 end
