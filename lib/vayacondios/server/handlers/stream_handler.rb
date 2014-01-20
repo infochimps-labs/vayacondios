a000d748dd89777f1ff4cad4fe4d54b5d4c3611a
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-20 17:22:15 -0600

    adding stream handler class

---
+++ lib/vayacondios/server/handlers/stream_handler.rb
@@ -0,0 +1,39 @@
+module Vayacondios::Server
+  class StreamHandler < EventsHandler
+
+    attr_reader :cursor, :timer, :on_data
+
+    def retrieve(params, query)
+      @timer = EM::Synchrony.add_periodic_timer(1){ stream_events }
+      @cursor = Event.receive(params).search(query)
+      Goliath::Response::STREAMING
+    end
+
+    def stream_data(&on_data)
+      @on_data = on_data
+      self
+    end
+
+    def close_stream!
+      timer.cancel
+    end
+
+    def update_cursor latest
+      cursor.filter.delete(:_t)
+      cursor.search(after: latest)
+    end
+    
+    def stream_events
+      log.debug 'Streaming events'
+      log.debug "  Stream cursor is #{cursor.filter}"
+      available = database.call(:search, cursor, cursor.filter.dup, {})
+      unless available.empty?
+        available.each do |result|
+          event = Event.new.format_response(result)
+          on_data.call event
+          update_cursor event[:time]
+        end
+      end
+    end
+  end
+end
