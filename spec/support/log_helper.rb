b91da9d496cfe8239af39913bd4e914059ef8d63
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-23 15:00:08 -0600

    added a simple log helper for capturing log output

---
+++ spec/support/log_helper.rb
@@ -0,0 +1,17 @@
+module LogHelper
+
+  def log
+    @logdev = StringIO.new
+    Logger.new @logdev
+  end
+
+  def log_device
+    @logdev
+  end
+
+  def log_content
+    log_device.rewind
+    log_device.read
+  end
+  
+end
