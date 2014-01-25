d78e8b71df369c389766afc8aafc79176c0e1f3e
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-24 21:21:56 -0600

    clean up spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,4 @@
-# Set this constant so we can easily find the configuration file to
-# launch Vayacondios with.
-VCD_ROOT = File.expand_path('../..', __FILE__)
-
 require 'bundler/setup' ; Bundler.require(:test)
-
-require 'configliere'
-
-Settings.define 'database.host',   default: 'localhost',        description: 'Hostname for database server'
-Settings.define 'database.port',   default: '27017',            description: 'Port for database server'
-Settings.define 'database.name',   default: 'vayacondios_test', description: 'Name of database to use'
-Settings.define 'database.driver', default: 'mongo',            description: 'Database driver'
-Settings.read File.expand_path('../config/spec.yml', __FILE__)
-Settings.resolve!
 
 require 'goliath/test_helper'
 
@@ -23,13 +10,14 @@ end
 require 'vayacondios-server'
 require 'vayacondios-client'
 
-Dir['spec/support/**/*.rb'].each{ |f| require File.join(File.dirname(__FILE__), '..', f) }
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{ |f| require f }
+
+Goliath.env = :test
+Vayacondios::Server::DbConfig.overlay(test: { name: 'peepee' })
 
 require 'vayacondios/server/api'
 
 RSpec.configure do |c|
+  c.include Goliath::TestHelper
   c.include DatabaseHelper
-
-  c.before(:each){ clean_db! }
-  c.after(:each) { clean_db! }
 end
