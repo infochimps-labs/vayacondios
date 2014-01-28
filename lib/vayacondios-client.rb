d1efd920ae6ace93cfe613e278b2ac7d971eab86
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-28 16:05:13 -0600

    reorganize require statements

--- lib/vayacondios-client.rb
+++ lib/vayacondios-client.rb
@@ -1,3 +1,11 @@
+require 'faraday'
+require 'faraday_middleware'
+require 'gorillib'
+require 'gorillib/metaprogramming/class_attribute'
+require 'gorillib/string/inflections'
+require 'logger'
 require 'multi_json'
-require 'vayacondios/client/client'
+
+require 'vayacondios/client/configuration'
+require 'vayacondios/client/http_methods'
 require 'vayacondios/client/http_client'
