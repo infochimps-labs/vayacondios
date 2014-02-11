a2af2f35df655fb1dbe979cd44257eee48b28d6c
Travis Dempsey <dempsey.travis@gmail.com>
2014-02-11 12:50:19 -0600

    require api options module; reorder

--- lib/vayacondios-server.rb
+++ lib/vayacondios-server.rb
@@ -17,17 +17,18 @@ require 'multi_json'
 
 require 'vayacondios'
 
-require 'vayacondios/server/driver'
+require 'vayacondios/server/api_options'
 require 'vayacondios/server/configuration'
+require 'vayacondios/server/driver'
 require 'vayacondios/server/drivers/mongo'
 
 require 'vayacondios/server/models/document'
-require 'vayacondios/server/models/stash'
 require 'vayacondios/server/models/event'
+require 'vayacondios/server/models/stash'
 
 require 'vayacondios/server/handlers/document_handler'
-require 'vayacondios/server/handlers/stash_handler'
-require 'vayacondios/server/handlers/stashes_handler'
 require 'vayacondios/server/handlers/event_handler'
 require 'vayacondios/server/handlers/events_handler'
+require 'vayacondios/server/handlers/stash_handler'
+require 'vayacondios/server/handlers/stashes_handler'
 require 'vayacondios/server/handlers/stream_handler'
