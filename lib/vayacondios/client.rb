196b47c351cb698a12749f12e3c221576a7e16ac
Travis Dempsey <dempsey.travis@gmail.com>
2014-02-13 15:17:56 -0600

    no longer need module level methods

--- lib/vayacondios/client.rb
+++ lib/vayacondios/client.rb
@@ -1,3 +1,4 @@
+require 'configliere'
 require 'faraday'
 require 'faraday_middleware'
 require 'gorillib'
@@ -7,34 +8,14 @@ require 'logger'
 require 'multi_json'
 
 require 'vayacondios'
-require 'configliere'
 require 'vayacondios/configuration'
 require 'vayacondios/client/configuration'
+require 'vayacondios/client/connection'
 require 'vayacondios/client/http_methods'
 require 'vayacondios/client/http_client'
 
 module Vayacondios
   module Client
 
-    module_function
-
-    def base_uri options
-      "http://#{options[:host] || ConnectionOpts[:host]}:#{options[:port] || ConnectionOpts[:port]}/#{Vayacondios::API_VERSION}"
-    end
-    
-    def new_connection(options = {})
-      Faraday.new(base_uri options) do |setup|
-        setup.adapter  options[:adapter] || ConnectionOpts[:adapter]
-        setup.request  :json
-        setup.response :json, content_type: /\bjson$/
-        if logger = options[:log] || ConnectionOpts[:log]
-          setup.response :logger, logger
-        end
-      end
-    end
-
-    def global_connection
-      @global_connection ||= new_connection
-    end
   end
 end
